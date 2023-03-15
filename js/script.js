6a544781050e80146c173ba9962618facfe969dd
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-15 18:34:35 +0100

    add for loop (comments)

--- js/script.js
+++ js/script.js
@@ -17,3 +17,9 @@ while (counter < shoppingList.length) {
     list.innerHTML += `<li>${shoppingList[counter]}</li>`;
     counter++;
 };
+
+// let listItem = '';
+// for (let a = 0; a < shoppingList.length; a++) {
+//     listItem += `<li>${shoppingList[a]}</li>`;
+// }
+// list.innerHTML += listItem;
